a9d368979fb1aa9602e1f90e64f1db0c80bba760
Martin Bianchi <tinchola7@gmail.com>
2019-03-25 07:36:09 -0300

    authentication bloc

---
+++ lib/bloc/authentication_bloc.dart
@@ -0,0 +1,39 @@
+import 'package:bloc/bloc.dart';
+import 'package:bloc_login/bloc/authentication_event.dart';
+import 'package:bloc_login/bloc/authentication_state.dart';
+import 'package:bloc_login/repositories/user_repository.dart';
+
+class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+
+  AuthenticationBloc({this.userRepository});
+
+  @override
+  AuthenticationState get initialState => AuthenticationUninitialized();
+
+  @override
+  Stream<AuthenticationState> mapEventToState(AuthenticationEvent event) async* {
+    if(event is AppStarted){
+      final bool hasToken = await userRepository.hasToken();
+
+      if(hasToken){
+        yield AuthenticationAuthenticated();
+      }
+      else{
+        yield AuthenticationUnauthenticated();
+      }
+    }
+
+    if(event is LoggedIn){
+      yield AuthenticationLoading();
+      await userRepository.persistToken(event.token);
+      yield AuthenticationAuthenticated();
+    }
+
+    if(event is LoggedOut){
+      yield AuthenticationLoading();
+      await userRepository.deleteToken();
+      yield AuthenticationUnauthenticated();
+    }
+  }
+}
